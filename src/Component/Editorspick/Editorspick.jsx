4c221befd8028a6ad46bb00d3790608f9be81252
pralad chy <praladchy28@gmail.com>
2022-09-28 19:01:49 +0545

    add Editorspick container

---
+++ src/Component/Editorspick/Editorspick.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LeftEditor from "../Trendingandeditor/Editor/LeftEditor"
+import "./Editorspick.css"
+const Editorspick = () => {
+
+  return (
+    <div className='editorspick-container'>
+        <LeftEditor />
+    </div>
+  )
+}
+
+export default Editorspick
