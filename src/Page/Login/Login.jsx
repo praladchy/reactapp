2ac6c4062b69285bab37eed54eb278fe56c9f97c
praladchy <praladchy028@gmail.com>
2023-02-07 17:10:06 +0545

    api fetch

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -1,31 +1,50 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+// import { NavLink } from 'react-router-dom'
 import Lable from '../../Shared/Lable/Lable'
 import './Login.css'
 const About = () => {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const submitForm = (e) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password
+    }
+    const response = fetch("http://localhost:8000/api/user/sign-in", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ data }),
+    });
+    console.log(response, 'response')
+  };
   return (
+
+
     <div>
       <div className='Login-main-Container'>
-        <h2><Lable Lable='Login'/></h2>
+        <h2><Lable Lable='Login' /></h2>
         <div className='Login-Container'>
-          <div className='Login-UserName-Container'>
-            <h4><Lable Lable='UserName' /></h4>
-            <input className='Input-Container' type="text,email" placeholder='UserName or Email' />
-          </div>
-          <div className='Login-Password-Container'>
-            <h4><Lable Lable='Password' /></h4>
-            <input className='Input-Container' type="password" placeholder='Password' />
-          </div>
-          <div className='Login-Cancle-Button-container'>
-            <div className='Login-button-Container'>
-              <NavLink to="/"> <button className='button-Container'>Login</button></NavLink>
+          <form action="" onSubmit={submitForm}>
+            <div className='Login-UserName-Container'>
+              <h4><Lable Lable='UserName' /></h4>
+              <input className='Input-Container' value={email} type="text,email" placeholder='UserName or Email' onChange={(e) => setEmail(e.target.value)} />
             </div>
-            <div className='Cancle-button-Container'>
-              <NavLink to="/"><button className='button-Container'>Cancle</button></NavLink>
+            <div className='Login-Password-Container'>
+              <h4><Lable Lable='Password' /></h4>
+              <input className='Input-Container' type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
             </div>
-          </div>
+            <div className='Login-Cancle-Button-container'>
+              <div className='Login-button-Container'>
+                <button className='button-Container' type="submit" >Login</button>
+              </div>
+              <div className='Cancle-button-Container'>
+                <button className='button-Container' >Cancle</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/Page/SignUp/SignUp.jsx
+++ src/Page/SignUp/SignUp.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 // import { Routing } from '../../Routings/Routing'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 import Lable from '../../Shared/Lable/Lable'
 import "./Signup.css"
+import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 
-const Home = () => {
+const SignUp = () => {
+  const[username,setUsername]=useState("")
+  const[password,setPassword]=useState("")
+  const[email,setEmail]=useState("")
+  const history=useNavigate()
+
+  const handleSignUp=async(e)=>{
+    e.preventDefault()
+   const response=await fetch('http://localhost:8000/api/user/sign-up',{
+      method:'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({username:username,password:password,email:email})
+    })
+    console.log(response.status,'response')
+   if(response.status===201){
+    history('/login')
+   }
+   if(response.status===400){
+    console.log(response.Response)
+   }
+  }
+
   return (
     <div className='Signup-Main-Container'>
       <div className='Signup-Sub-Container'>
@@ -12,22 +35,35 @@ const Home = () => {
         <div className='Signup-Container'>
           <div className='Signup-UserName-Container'>
             <h4>UserName</h4>
-            <input className='Input-Container' type="text" placeholder='UserName ' />
+            <input className='Input-Container'
+             type="text"
+              placeholder='UserName '
+              onChange={(e)=>setUsername(e.target.value)}
+               />
           </div>
           <div className='Signup-Email-Container'>
             <h4>Email</h4>
-            <input className='Input-Container' type="email" placeholder='Example@gmail.com' />
+            <input className='Input-Container'
+             type="email"
+              placeholder='Example@gmail.com'
+              onChange={(e)=>setEmail(e.target.value)}
+              />
           </div>
           <div className='Signup-Password-Container'>
             <h4>Password</h4>
-            <input className='Input-Container' type="password" placeholder='Password' />
+            <input 
+            className='Input-Container'
+             type="password" 
+             placeholder='Password' 
+             onChange={(e)=>setPassword(e.target.value)}
+             />
           </div>
           <div className='Signup-Cancle-Button-container'>
             <div className='Signup-button-Container' >
-              <NavLink to="/"> <button className='button-Container'>Create</button></NavLink>
+               <button className='button-Container'onClick={handleSignUp}>Create</button>
             </div>
             <div className='Cancle-button-Container'>
-              <NavLink to="/"><button className='button-Container'>Cancle</button></NavLink>
+            <button className='button-Container'>Cancle</button>
             </div>
           </div>
         </div>
@@ -37,4 +73,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default SignUp
